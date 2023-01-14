10732c36661cb2ea62582f48e650e28245d8b048
Garblovian <zulus69@gmail.com>
2023-01-14 15:48:22 +0300

    decoder with associations text file

---
+++ decoder.rb
@@ -0,0 +1,23 @@
+matches = {}
+File.foreach('associations.txt') do |a|
+  split = a.split('|')
+  matches[split[0]] = split[1].strip!
+end
+
+# puts matches.inspect
+
+File.foreach("credits.txt") do |line| 
+  word_arr = []
+  line.split('').each do |char|
+    if char != ' '
+      if matches[char]
+        word_arr.push(matches[char])
+      else
+        word_arr.push(char)
+      end
+    else
+    word_arr.push(char)
+    end
+  end
+  puts word_arr.join('')
+end
